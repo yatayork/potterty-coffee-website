17e2c68ff4143a6641f4d191a62343d1ab562678
Yatayork <103381337+yatayork@users.noreply.github.com>
2024-06-26 20:18:46 +0800

    dist

---
+++ dist/assets/AboutView-BkwElgJD.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-CNLkbpKU.js";const t={};function n(r,s){return o(),c("h2",null,"about us")}const _=e(t,[["render",n]]);export{_ as default};
--- dist/assets/AboutView-De72XKN0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-ClpWGwVQ.js";const t={};function n(r,s){return o(),c("h2",null,"about us")}const _=e(t,[["render",n]]);export{_ as default};
